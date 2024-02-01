a7aed80157dcad7f6898864741f8508bd7b3a3e1
aaryanpunia <aaryan57231punia@gmail.com>
2024-01-31 17:21:40 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/mergeservices/goose-custom
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
+	github.com/pressly/goose/v3 v3.18.0
+)
+
+require (
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+// This is custom goose binary with sqlite3 support only.
+
+package main
+
+import (
+	"flag"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/pressly/goose/v3"
+	"log"
+	"os"
+)
+
+var (
+	flags = flag.NewFlagSet("goose", flag.ExitOnError)
+	dir   = flags.String("dir", ".", "directory with migration files")
+)
+
+func main() {
+	flags.Parse(os.Args[1:])
+	args := flags.Args()
+	log.Default().Printf("%v", args)
+	if len(args) < 1 {
+		flags.Usage()
+		return
+	}
+
+	command := args[0]
+	log.Default().Printf("%v", command)
+
+	dbstring, exists := os.LookupEnv("DSN")
+
+	if !exists {
+		log.Fatal("DB Connection string not set")
+	}
+
+	db, err := goose.OpenDBWithDriver("mysql", dbstring)
+	if err != nil {
+		log.Fatalf("goose: failed to open DB: %v\n", err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatalf("goose: failed to close DB: %v\n", err)
+		}
+	}()
+
+	arguments := []string{}
+	if len(args) > 1 {
+		arguments = append(arguments, args[1:]...)
+	}
+
+	if err := goose.Run(command, db, *dir, arguments...); err != nil {
+		log.Fatalf("goose %v: %v", command, err)
+	}
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env", err)
+	}
+}
